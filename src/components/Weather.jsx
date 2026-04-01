99eb842925cb60984f3f2a60e88d1ef9b85ad256
Prakhar Upadhyay <prakharupadhyay407@gmail.com>
2026-04-01 16:11:35 +0530

    debug env

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -37,6 +37,8 @@ const Weather = () => {
             return;
         }
         try {
+            console.log("KEY:", import.meta.env.VITE_APP_ID);
+            
             const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
             const response = await fetch(url);
